ae28c008eb9ee43785976b85b01f5a07c55525ca
Alexander Kochemasov <akochemasov@gmail.com>
2018-10-14 09:16:32 +0400

    fixes test delete

--- test/functional/delete-post.spec.js
+++ test/functional/delete-post.spec.js
@@ -1,6 +1,5 @@
 const Factory = use('Factory')
 const Post = use('App/Models/Post')
-const PostController = use('App/Controllers/Http/PostController')
 const { test, trait } = use('Test/Suite')('Delete Post')
 
 trait('Auth/Client')
@@ -9,7 +8,7 @@ trait('Test/ApiClient')
 
 test('we can delete a post', async ({ assert, client }) => {
   const post = await Factory.model('App/Models/Post').create()
-  
+
   const response = await client.get(`posts/${post.id}/delete`).end()
 
   response.assertStatus(200)
